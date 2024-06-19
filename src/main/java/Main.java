82898dcad224d13c9b6c3b175314c499bf350ca4
BartoszS731 <161606110+BartoszS731@users.noreply.github.com>
2024-06-19 12:48:46 +0000

    kolokwium
    program

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,37 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Scanner;
 
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 // import org.junit.jupiter.api.Test;
+// 33. Napisz program, który znajduje drugą najniższą liczbę w tablicy:
+//- Program powinien wczytać tablicę liczb całkowitych wprowadzoną przez użytkownika.
+//- Znaleźć drugą najniższą liczbę w tablicy.
+//- Wyświetlić wynik.
 
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
-
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj liczbę elementów w tablicy: ");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("nadajliczbe");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (array[i] < smallest) {
+                secondSmallest = smallest;
+                smallest = array[i];
+            } else if (array[i] < secondSmallest && array[i] != smallest) {
+                secondSmallest = array[i];
+            }
+        }
+        if (secondSmallest == Integer.MAX_VALUE) {
+            System.out.println("Nieistnieje taka liczba");
+        } else {
+            System.out.println("Druga najmniejsza liczba w tablicy: " + secondSmallest);
+        }
+    }
 }
